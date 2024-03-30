e9e6d5864e099ede063dea9c0acaeb1952b5c3d7
user <raz_etka_2010@mail.ru>
2024-03-30 18:05:03 +0500

    pog

---
+++ BaseKotlin/src/main/kotlin/01_Lesson.kt
@@ -0,0 +1,129 @@
+import kotlin.math.PI
+import kotlin.math.pow
+
+fun main() {
+//   var greeting = "Hello!";
+//    greeting = "Hi!"
+// var name =  "Артём"
+//    var age = "много"
+//    println("$greeting моё имя $name, $age лет")
+////   greeting = 10
+// var number: Number = 10
+//
+//   print(1)
+// print(2)
+//    println(number)
+//    println("$greeting: pog $number")
+//    var hours = 24
+//    hours = 25
+//
+//        /*
+//ага
+//         */
+// var x = 8
+//    x = x + 4
+//    x += 4
+//    x -= 4
+//    x /=2
+//    x *= 8
+//    println(x)
+//
+//
+//    var radius = 7
+//    val PI = 3.14
+//   var circleArea = PI * radius * radius
+//   println(circleArea)
+//var circleArea2 = PI.toInt() * radius * radius
+//    println(circleArea2 )
+//    var radiusD: Double = 7.0
+//    var circleArea3 = PI * radiusD.pow(2)
+//    println(PI)
+//   println(circleArea3)
+//
+////    fun main(args: Array<String>) {
+////
+////       for (i in 1..5) {
+////           println(i)
+////       }
+////   }
+//
+//    fun main(args: Array<String>) {
+//
+//        var language = arrayOf("Ruby", "Kotlin", "Python", "Java")
+//
+//        for (item in language)
+//      println(item)
+//    }
+//
+//var friends = listOf("я","он")
+//    for (item in friends){
+//        if (item != "он")
+//            println(item)
+//    }
+//for(i in name)
+//    println(i)
+//
+//    for (i in 1..10 step  2 )
+//        println(i)
+//
+//for(x in 10 downTo 0)
+//    println(x)
+//    println("pog")
+
+
+
+//        var i = 1
+//    while (i <= 100) {
+//        if (i % 2 == 0) {
+//            println(i)
+//        }
+//    }
+
+//var number = 100
+//
+//while (number >= 0) {
+//    if (number % 2 == 1) {
+//        println(number)
+//    }
+//
+//    number - 1
+//
+//var i = 0
+//    while (i <= 10) {
+//    if (i % 2 == 0) {
+//    println(i)
+//     }
+//    i++
+//    }
+
+//    var f =8
+//    1 * 2 * 3 * 4 * 5 * 6 * 7 * 8
+//    var factorial = 1
+//    for (i in 1..8){
+//        factorial *= i
+//    }
+//    println("факториал числа $f = $factorial")
+
+//    for (i in 1..10) {
+//        for (j in 1..10) {
+//            print(i * j)
+//            print("")
+//        }
+//        println()
+//    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
